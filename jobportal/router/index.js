64a54664edef7ccd399481381a63331972557507
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2023-07-03 07:09:59 +0000

    clearing cache

--- jobportal/router/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homecontrol = require('../controller/Homepage');
-router.get('/',homecontrol.home);
-
-//User
-router.use('/user',require('./user'));
-module.exports = router;
--- jobportal/router/user.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userControl = require('../controller/userpage');
-router.get('/signup',userControl.signup);
-
-//User
-module.exports = router;
